2eb79afd03a69838cf3256e0cea4f89dea009533
gepron1x <georgijpronuk@gmail.com>
2024-10-09 01:21:25 +0300

    working youtube config

--- zapret-winws/preset_gepron1x.cmd
+++ zapret-winws/preset_gepron1x.cmd
@@ -2,6 +2,6 @@ start "zapret: http,https,quic" /min "%~dp0winws.exe" ^
 --wf-tcp=80,443 --wf-udp=443,50000-65535 ^
 --filter-udp=443 --hostlist="%~dp0list-youtube.txt" --dpi-desync=fake --dpi-desync-repeats=10 --new ^
 --filter-tcp=443 --hostlist="%~dp0list-discord.txt" --dpi-desync=disorder2 --new ^
---filter-tcp=443 --hostlist="%~dp0list-others.txt"  --dpi-desync=split2 --dpi-desync-split-tls=sniext --new ^
---filter-tcp=443 --hostlist="%~dp0list-youtube.txt" --dpi-desync=fake,split2 --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig --dpi-desync-fake-tls="%~dp0tls_clienthello_www_google_com.bin" --new ^
+--filter-tcp=443 --hostlist="%~dp0list-others.txt"  --dpi-desync=disorder2 --new ^
+--filter-tcp=443 --hostlist="%~dp0list-youtube.txt" --dpi-desync=split --dpi-desync-fooling=badseq --new ^
 --filter-udp=50000-65535 --dpi-desync=fake --dpi-desync-cutoff=d4 --dpi-desync-repeats=6 --dpi-desync-any-protocol

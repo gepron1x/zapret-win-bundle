c70a47e7a915414757fbfff8d005119935bcdb2e
gepron1x <georgijpronuk@gmail.com>
2024-10-09 01:26:03 +0300

    service creation

--- zapret-winws/service_create.cmd
+++ zapret-winws/service_create.cmd
@@ -1,4 +1,9 @@
-set ARGS=--wf-l3=ipv4,ipv6 --wf-tcp=80,443 --dpi-desync=fake,split --dpi-desync-ttl=7 --dpi-desync-fooling=md5sig
+set ARGS=--wf-tcp=80,443 --wf-udp=443,50000-65535 ^
+--filter-udp=443 --hostlist="%~dp0list-youtube.txt" --dpi-desync=fake --dpi-desync-repeats=10 --new ^
+--filter-tcp=443 --hostlist="%~dp0list-discord.txt" --dpi-desync=disorder2 --new ^
+--filter-tcp=443 --hostlist="%~dp0list-others.txt"  --dpi-desync=disorder2 --new ^
+--filter-tcp=443 --hostlist="%~dp0list-youtube.txt" --dpi-desync=split --dpi-desync-fooling=badseq --new ^
+--filter-udp=50000-65535 --dpi-desync=fake --dpi-desync-cutoff=d4 --dpi-desync-repeats=6 --dpi-desync-any-protocol
 call :srvinst winws1
 rem set ARGS=--wf-l3=ipv4,ipv6 --wf-udp=443 --dpi-desync=fake 
 rem call :srvinst winws2

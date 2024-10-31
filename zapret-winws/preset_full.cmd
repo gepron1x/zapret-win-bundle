b8cc789a40b7ef150e5db75b539264f69dae1457
gepron1x <georgijpronuk@gmail.com>
2024-11-01 01:09:43 +0300

    update discord lists

--- zapret-winws/preset_full.cmd
+++ zapret-winws/preset_full.cmd
@@ -1,7 +1,5 @@
 start "zapret: http,https,quic" /min "%~dp0winws.exe" ^
 --wf-raw="@%~dp0rules.txt" ^
---filter-udp=443 --hostlist="%~dp0list-youtube.txt" --dpi-desync=fake --dpi-desync-repeats=10 --new ^
---filter-tcp=443 --hostlist="%~dp0list-discord.txt" --dpi-desync=disorder2 --new ^
---filter-tcp=443 --hostlist="%~dp0list-others.txt"  --dpi-desync=disorder2 --new ^
---filter-tcp=443 --hostlist="%~dp0list-youtube.txt" --dpi-desync=split --dpi-desync-fooling=badseq --new ^
+--filter-udp=443 --dpi-desync=fake --dpi-desync-repeats=10 --dpi-desync-fake-quic="%~dp0quic_initial_www_google_com.bin" --new ^
+--filter-tcp=443 --hostlist="%~dp0list-youtube.txt" --hostlist="%~dp0list-discord.txt" --hostlist="%~dp0list-others.txt" --dpi-desync=split --dpi-desync-repeats=10 --dpi-desync-fooling=badseq --dpi-desync-split-pos=1 --dpi-desync-fake-tls=0x1603030135010001310303424143facf5c983ac8ff20b819cfd634cbf5143c0005b2b8b142a6cd335012c220008969b6b387683dedb4114d466ca90be3212b2bde0c4f56261a9801 --new ^
 --filter-udp=50000-65535 --dpi-desync=fake --dpi-desync-cutoff=d4 --dpi-desync-repeats=6 --dpi-desync-any-protocol
--- zapret-winws/service_create.cmd
+++ zapret-winws/service_create.cmd
@@ -1,6 +1,6 @@
 set ARGS=--wf-raw="@%~dp0rules.txt" ^
---filter-udp=443 --hostlist="%~dp0list-youtube.txt" --dpi-desync=fake --dpi-desync-udplen-increment=10 --dpi-desync-repeats=6 --dpi-desync-udplen-pattern=0xDEADBEEF --dpi-desync-fake-quic="%~dp0quic_initial_www_google_com.bin" --new ^
---filter-tcp=443 --hostlist="%~dp0list-youtube.txt" --hostlist="%~dp0list-discord.txt" --hostlist="%~dp0list-others.txt"  --dpi-desync=split --dpi-desync-fooling=badseq --dpi-desync-split-pos=1 --new ^
+--filter-udp=443 --dpi-desync=fake --dpi-desync-repeats=10 --dpi-desync-fake-quic="%~dp0quic_initial_www_google_com.bin" --new ^
+--filter-tcp=443 --hostlist="%~dp0list-youtube.txt" --hostlist="%~dp0list-discord.txt" --hostlist="%~dp0list-others.txt" --dpi-desync=split --dpi-desync-repeats=10 --dpi-desync-fooling=badseq --dpi-desync-split-pos=1 --dpi-desync-fake-tls=0x1603030135010001310303424143facf5c983ac8ff20b819cfd634cbf5143c0005b2b8b142a6cd335012c220008969b6b387683dedb4114d466ca90be3212b2bde0c4f56261a9801 --new ^
 --filter-udp=50000-65535 --dpi-desync=fake --dpi-desync-cutoff=d4 --dpi-desync-repeats=6 --dpi-desync-any-protocol
 call :srvinst winws1
 rem set ARGS=--wf-l3=ipv4,ipv6 --wf-udp=443 --dpi-desync=fake 
